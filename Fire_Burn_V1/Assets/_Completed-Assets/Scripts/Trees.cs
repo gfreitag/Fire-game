2737fcd35e72187c18d63232a9b5baa08ca5618f
杨 <xiaoyang@yangdeMacBook-Pro.local>
2019-02-02 20:36:45 -0500

    edited fireman and trees

---
+++ Fire_Burn_V1/Assets/_Completed-Assets/Scripts/Trees.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trees : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObject = new GameObject("Tree");
+        newObject.AddComponent<Image>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
